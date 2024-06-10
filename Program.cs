dbe24ed1fe29ccde4d4f431c0b82c32549da93c7
Алексей Лейс <vipaleksei007@gmail.com>
2024-06-10 11:49:11 +0300

    new

--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
-﻿using System.IO;
-namespace less5
+﻿namespace less5
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            Console.Write("Добро пожаловать на работу по курсу №5 \n введитие номер задания 1 - 3:");
+            Console.Write("Добро пожаловать на работу по курсу №5 \n введитие номер задания 1 - 3");
             int numTask = Convert.ToInt32(Console.ReadLine());
             switch (numTask)
             {
@@ -18,10 +16,10 @@ namespace less5
                     break;
                 case 2:
                     Console.WriteLine("Откройте файл startup.txt");
-                    var dt_h =  DateTime.Now.Hour;
+                    var dt_h = DateTime.Now.Hour;
                     var dt_m = DateTime.Now.Minute;
                     var dt_s = DateTime.Now.Second;
-                    File.WriteAllText(@"D:\\LAV\\Files\\Programming\\Plan\\less5\\textfiles\startup.txt", dt_h.ToString() +":" + dt_m.ToString()+":" + dt_s.ToString(), System.Text.Encoding.UTF8);
+                    File.WriteAllText(@"D:\\LAV\\Files\\Programming\\Plan\\less5\\textfiles\startup.txt", dt_h.ToString() + ":" + dt_m.ToString() + ":" + dt_s.ToString(), System.Text.Encoding.UTF8);
                     break;
                 case 3:
                     byte[] bytesArr = new byte[10];
@@ -30,16 +28,17 @@ namespace less5
                     {
                         bytesArr[i] = Convert.ToByte(Console.ReadLine());
                     }
-                    File.WriteAllBytes(@"D:\\LAV\\Files\\Programming\\Plan\\less5\\textfiles\byteARR.bin", bytesArr );
+                    File.WriteAllBytes(@"D:\\LAV\\Files\\Programming\\Plan\\less5\\textfiles\byteARR.bin", bytesArr);
                     break;
                 default:
-                    Console.WriteLine("Нет такого номера задания!");
+                    Console.WriteLine("[sys_mes]: Нет такого номера задания!");
                     break;
             }
-
-
 
         }
 
     }
+
 }
+
+
